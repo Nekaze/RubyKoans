366fae0a7474677d732ca17480e599ad9fad806e
Nekaze <53487352+Nekaze@users.noreply.github.com>
2021-06-05 10:37:56 -0300

    Finished about_triangle_projects and about_exceptions. Progress 57%

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,20 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  # WRITE THIS CODE   
+  sides = [a,b,c].sort
+  if sides[0] > 0 && sides[2] < sides[0] + sides[1] 
+    case sides.uniq.length
+    when 1
+      :equilateral
+    when 2
+      :isosceles
+    when 3
+      :scalene
+    end
+  else
+    raise TriangleError
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
